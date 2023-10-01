cb4bd8a1b5e3ca0ff7af0187b360b9325c018e50
Diya Bhandari <diyabhandari@Diyas-MacBook-Pro.local>
2023-10-01 13:13:21 +0100

    Add milestone_2.py with favorite fruits list

---
+++ milestone_2.py
@@ -0,0 +1,9 @@
+
+# Step 1: Create a list of 5 favorite fruits
+favorite_fruits = ["Apple", "Banana", "Orange", "Strawberry", "Grapes"]
+
+# Step 2: Assign the list to a variable called word_list
+word_list = favorite_fruits
+
+# Step 3: Print out the newly created list
+print(word_list)
